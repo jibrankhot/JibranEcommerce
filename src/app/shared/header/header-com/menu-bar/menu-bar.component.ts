17347ab54b009156a87b247efd5cd662e5200e72
jibran <jibrankhot5@gmail.com>
2025-08-12 18:01:15 +0530

    dropdown working

--- src/app/shared/header/header-com/menu-bar/menu-bar.component.ts
+++ src/app/shared/header/header-com/menu-bar/menu-bar.component.ts
@@ -1,15 +1,14 @@
-
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../../shared.module';
 import { IMenuItem } from '../../../types/menu-d-type';
 import { menu_data } from '../../../data/menu-data';
 
 @Component({
-    selector: 'app-menu-bar',
-    templateUrl: './menu-bar.component.html',
-    styleUrls: ['./menu-bar.component.scss'],
-    imports: [SharedModule]
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss'],
+  imports: [SharedModule]
 })
 export class MenuBarComponent {
-  public menu_data: IMenuItem[] = menu_data
+  public menu_data: IMenuItem[] = menu_data;
 }
