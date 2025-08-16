b999635daf5e35f8a7b3894e1778af5155e92c69
jibran <jibrankhot5@gmail.com>
2025-08-16 19:43:29 +0530

    .

--- src/app/shared/header/header-com/menu-bar/menu-bar.component.ts
+++ src/app/shared/header/header-com/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SharedModule } from '../../../../shared.module';
 import { IMenuItem } from '../../../types/menu-d-type';
 import { menu_data } from '../../../data/menu-data';
@@ -10,5 +10,23 @@ import { menu_data } from '../../../data/menu-data';
   imports: [SharedModule]
 })
 export class MenuBarComponent {
+  @Input() isMobile: boolean = false;
+
   public menu_data: IMenuItem[] = menu_data;
+  public activeTab: IMenuItem | null = this.menu_data[0] ?? null;
+
+  public openSubmenuId: string | number | null = null;
+
+  toggleTab(item: IMenuItem) {
+    this.activeTab = this.activeTab?.id === item.id ? null : item;
+    this.openSubmenuId = null;
+  }
+
+  toggleSubmenu(submenuId: string | number) {
+    this.openSubmenuId = this.openSubmenuId === submenuId ? null : submenuId;
+  }
+
+  isSubmenuOpen(submenuId: string | number): boolean {
+    return this.openSubmenuId === submenuId;
+  }
 }
